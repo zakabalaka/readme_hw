e057ec642ef26029ba86bd22990a42acded5b3eb
mckenzie balaka <zakabalaka@yahoo.com>
2024-12-11 22:18:45 -0500

    Update all files

--- index.js
+++ index.js
@@ -1,87 +1,125 @@
-// Declaring the dependencies and variables
-const fs = require("fs");
-const util = require("util");
+// TODO: Include packages needed for this application
+const fs = require('fs');
 const inquirer = require('inquirer');
-const generateReadme = require("./utils/generateReadme")
-const writeFileAsync = util.promisify(fs.writeFile);
+const generateMarkdown = require('./generateMarkdown.js');
 
-//Prompt the user questions to create the README.md
-function promptUser(){
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "projectTitle",
-            message: "What is the project title?",
-        },
-        {
-            type: "input",
-            name: "description",
-            message: "Write a brief description of your project: "
-        },
-        {
-            type: "input",
-            name: "installation",
-            message: "Describe the installation process if any: ",
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: "What is this project usage for?"
-        },
-        {
-            type: "list",
-            name: "license",
-            message: "Chose the appropriate license for this project: ",
-            choices: [
-                "Apache",
-                "Academic",
-                "GNU",
-                "ISC",
-                "MIT",
-                "Mozilla",
-                "Open"
-            ]
-        },
-        {
-            type: "input",
-            name: "contributing",
-            message: "Who are the contributors of this project?"
-        },
-        {
-            type: "input",
-            name: "tests",
-            message: "Is there a test included?"
-        },
-        {
-            type: "input",
-            name: "questions",
-            message: "What do I do if I have an issue? "
-        },
-        {
-            type: "input",
-            name: "username",
-            message: "Please enter your GitHub username: "
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter your email: "
-        }
-    ]);
-} 
-
-// Async function using util.promisify 
-async function init() {
-    try {
-        // Ask user questions and generate responses
-        const answers = await promptUser();
-        const generateContent = generateReadme(answers);
-        // Write new README.md to dist directory
-        await writeFileAsync('./dist/README.md', generateContent);
-        console.log('✔️  Successfully wrote to README.md');
-    }   catch(err) {
-        console.log(err);
-    }
+// TODO: Create an array of questions for user input
+const formatEntry = ({ entry, date }) => {
+  return `# ${date}
+${entry}
+`;
 }
 
-init();  
+inquirer
+  .prompt(
+
+    [
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?\n",
+      },
+      {
+        type: "input",
+        name: "repoTitle",
+        message: "What is the exact title of your repository?\n",
+      },
+      {
+        type: "editor",
+        name: "description",
+        message:
+          "\nPlease enter a description of your project: \nExplain the what, why, and how of your project. \nUse the following questions as a guide: \n\t- What was your motivation? \n\t- Why did you build this project? \n\t- What problem does it solve? \n\t- What did you learn?\n",
+      },
+      {
+        type: "input",
+        name: "deployedWebsite",
+        message: "Please enter deployed website URL:\n",
+      },
+      {
+        type: "input",
+        name: "imgAlt",
+        message: "Please enter your screenshot Alt:\n",
+      },
+      {
+        type: "input",
+        name: "screenshot",
+        message: "Please enter relative path to screenshot:\n",
+      },
+      {
+        type: "input",
+        name: "screenshotSubtitle",
+        message: "Please enter screenshot subtitle:\n",
+      },
+      {
+        type: "editor",
+        name: "installation",
+        message: "Please enter installation instructions for your project:\n",
+      },
+      {
+        type: "input",
+        name: "librariesAndTools",
+        message:
+          "Please enter libraries and tools used for your project. Write them in a list like this: \n<li>First item</li> <li>Second item</li> <li>Third item</li>:\n",
+      },
+      {
+        type: "editor",
+        name: "usage",
+        message: "Please enter usage information for your project:\n",
+      },
+      {
+        type: "list",
+        name: "license",
+        message:
+          "\n Please choose a license for your project\n (Visit https://choosealicense.com/ if you are unsure, which license to choose):",
+        choices: [
+          "MIT",
+          "GPL-3-0",
+          "apache-2-0",
+          "BSD-3-clause",
+          "ISC",
+          "0bsd",
+        ],
+      },
+
+      {
+        type: "editor",
+        name: "contribution",
+        message: "\nPlease enter contribution guidelines for your project:\n",
+      },
+      {
+        type: "editor",
+        name: "tests",
+        message: "Please enter test instructions for your project:\n",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?\n",
+      },
+      {
+        type: "input",
+        name: "website",
+        message: "What is your website URL?\n",
+      },
+      {
+        type: "input",
+        name: "webLinkPlaceholder",
+        message: "What is the placeholder for the website URL?\n",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?\n",
+      },
+    ]
+)
+
+.then((answers) => {
+    console.log(answers);
+
+    fs.writeFile(`README.md`, generateMarkdown(answers), (error) =>
+error
+        ? console.error(error)
+        : console.log(`Success! You have now created a README.md `)
+    );
+});
